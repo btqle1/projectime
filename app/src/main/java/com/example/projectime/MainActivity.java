a7f554e60fcc1e727bd2cb9f96bb76a6078038e0
bqle1 <btqle1@gmail.com>
2019-11-06 01:29:58 -0600

    Added basic toolbar, created PTModules, created an FAB that takes you to PTModules

--- app/src/main/java/com/example/projectime/MainActivity.java
+++ app/src/main/java/com/example/projectime/MainActivity.java
@@ -1,8 +1,20 @@
 package com.example.projectime;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ShareActionProvider;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuItemCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+/*
+MUST add to project structure:
+Design
+*/
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +22,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    public void onClickDone(View view){
+        Intent intent = new Intent(this, PTModules.class);
+        startActivity(intent);
     }
 }
