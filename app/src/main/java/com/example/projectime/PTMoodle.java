67eeccbc19b5a12188e69b9b4b275f9ce6a07e14
Your Name <you@example.com>
2019-11-11 23:26:03 +0000

    Nav to PT Event List

--- app/src/main/java/com/example/projectime/PTMoodle.java
+++ app/src/main/java/com/example/projectime/PTMoodle.java
@@ -7,6 +7,7 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
@@ -34,9 +35,26 @@ public class PTMoodle extends AppCompatActivity {
         cv.put("NAME", "CSCI"+(rng.nextInt(9000)+1000));
         cv.put("COLOR", rng.nextInt(1<<24));
 
-        db.insert("CALENDAR",null,cv);
+        long calendarId = db.insert("CALENDAR",null,cv);
+
+        cv = new ContentValues();
+        cv.put("CALENDAR_ID", calendarId);
+        cv.put("NAME", "Sample Tab");
+
+        long tabId = db.insert("TAB", null, cv);
+
+        long time = System.currentTimeMillis() / 1000L + 3600L;
+
+        cv = new ContentValues();
+        cv.put("TAB_ID", tabId);
+        cv.put("NAME", "Sample Event");
+        cv.put("TIME", time);
+        cv.put("URI", "https://reddit.com/");
+
+        long eventId = db.insert("EVENT",null,cv);
 
         db.close();
+
 
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
