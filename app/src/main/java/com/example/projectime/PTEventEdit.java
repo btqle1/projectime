0a20cf78dd542fbaf3f90bf802e1ccea9d741c9c
bqle1 <bqle1@uno.edu>
2019-12-11 19:11:15 -0600

    Eventedit can now cahnge the name and the due date properly and it reflects in the database

--- app/src/main/java/com/example/projectime/PTEventEdit.java
+++ app/src/main/java/com/example/projectime/PTEventEdit.java
@@ -10,6 +10,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -17,6 +18,8 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class PTEventEdit extends AppCompatActivity {
 
@@ -30,6 +33,11 @@ public class PTEventEdit extends AppCompatActivity {
     private long eventID;
     private long tabID;
     private long calendarID;
+    private long date;
+    private Calendar dueDate;
+    Spinner daySpinner;
+    Spinner monthSpinner;
+    Spinner yearSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,17 +51,41 @@ public class PTEventEdit extends AppCompatActivity {
         int intEventID = (int)eventID;
         tabID = getIntent().getLongExtra(EXTRA_TAB_ID, -1);
         calendarID = getIntent().getLongExtra(EXTRA_CALENDAR_ID, -1);
+        dueDate = new GregorianCalendar();
+        String[] strDaysArray = initDayArray();
+        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>
+                (this,android.R.layout.simple_spinner_item, strDaysArray);
+        daySpinner = (Spinner) findViewById(R.id.spinner_event_day);
+        monthSpinner = (Spinner) findViewById(R.id.spinner_event_month);
+        yearSpinner = (Spinner) findViewById(R.id.spinner_event_year);
+
+        daySpinner.setAdapter(spinnerArrayAdapter);
         try{
             dbHelper = new PTDatabaseHelper(this);
             db = dbHelper.getReadableDatabase();
             cursor = db.query("EVENT",
-                    new String[]{"_id", "NAME","CALENDAR_ID","TAB_ID"},
+                    new String[]{"_id", "NAME","CALENDAR_ID","TAB_ID", "TIME"},
                     "_id = ? AND TAB_ID = ? AND CALENDAR_ID = ?",
                     new String[]{String.valueOf(eventID), String.valueOf(tabID), String.valueOf(calendarID)},
                     null, null, null);
 
             if(cursor.moveToFirst()){
                 String name = cursor.getString(cursor.getColumnIndex("NAME"));
+                date = cursor.getLong(cursor.getColumnIndex("TIME"));
+                Log.i("info", "Time is " + date);
+                GregorianCalendar gCal = new GregorianCalendar();
+                gCal.setTimeInMillis(date);
+                int year = gCal.get(GregorianCalendar.YEAR);
+                int month = gCal.get(GregorianCalendar.MONTH);
+                int day = gCal.get(GregorianCalendar.DAY_OF_MONTH);
+                Log.i("info", "Year is " + year);
+                Log.i("info", "Month is " + month);
+                Log.i("info", "Day is " + day);
+
+                daySpinner.setSelection(day -1);
+                monthSpinner.setSelection(month);
+                yearSpinner.setSelection(0);
+
                 EditText editText = (EditText) findViewById(R.id.edit_event_name);
                 editText.setText(name);
             }
@@ -61,18 +93,21 @@ public class PTEventEdit extends AppCompatActivity {
             Toast toast = Toast.makeText(this, "database unavailable", Toast.LENGTH_SHORT);
             toast.show();
         }
-        String[] strDaysArray = initDayArray();
-        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>
-                (this,android.R.layout.simple_spinner_item, strDaysArray);
-        Spinner daySpinner = (Spinner) findViewById(R.id.spinner_event_day);
-        daySpinner.setAdapter(spinnerArrayAdapter);
     }
 
     public void onConfirmEdit(View view){
         EditText eventNameField = (EditText) findViewById(R.id.edit_event_name);
         String eventName = eventNameField.getText().toString();
+        int day = daySpinner.getSelectedItemPosition()+1;
+        int month = monthSpinner.getSelectedItemPosition();
+        int year = 2019;
+        Calendar dueDate = new GregorianCalendar(year, month, day);
+        long date = dueDate.getTimeInMillis();
+
+
         cv = new ContentValues();
         cv.put("NAME", eventName);
+        cv.put("TIME", date);
         try{
             SQLiteOpenHelper dbHelper = new PTDatabaseHelper(this);
             db = dbHelper.getWritableDatabase();
